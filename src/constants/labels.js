d198d2b19d68d0b79182ce146a071af6e16f3329
はこだてたろう <hakodate.1106v@gmail.com>
2022-12-15 13:08:24 +0900

    コミット漏れの追加

---
+++ src/constants/labels.js
@@ -0,0 +1,4 @@
+export const PostsInTheSameCategory = '同じカテゴリーの記事'
+export const RecommendedPosts = 'おすすめの記事'
+export const LatestPosts = '最近の記事'
+export const Tag = 'タグ'
--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -28,7 +28,7 @@ import {
   getAllTags,
   getAllBlocksByBlockId,
 } from '../../lib/notion/client'
-import {LatestPosts, PostsInTheSameCategory, RecommendedPosts, Tag} from "../../constants/labels";
+import * as Labels from '../../constants/labels'
 
 export async function getStaticProps({ params: { slug } }) {
   const post = await getPostBySlug(slug)
@@ -155,12 +155,12 @@ const RenderPost = ({
 
       <div className={styles.subContent}>
         <BlogPostLink
-          heading={PostsInTheSameCategory}
+          heading={Labels.PostsInTheSameCategory}
           posts={sameTagPosts}
         />
-        <BlogPostLink heading={RecommendedPosts} posts={rankedPosts} />
-        <BlogPostLink heading={LatestPosts} posts={recentPosts} />
-        <BlogTagLink heading={Tag} tags={tags} />
+        <BlogPostLink heading={Labels.RecommendedPosts} posts={rankedPosts} />
+        <BlogPostLink heading={Labels.LatestPosts} posts={recentPosts} />
+        <BlogTagLink heading={Labels.Tag} tags={tags} />
       </div>
     </div>
   )
